f23406723ee675179bad14fc4ee015386a6e0d13
Melody Yu <mjy013@ucsd.edu>
2021-01-14 23:00:31 -0800

    added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,8 +2,18 @@ $(document).ready(function() {
     initializePage();
 });
 
+//changes color of project node 
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#d6cced");
+}
+
 function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    $("a.thumbnail").click(projectClick);
 }
